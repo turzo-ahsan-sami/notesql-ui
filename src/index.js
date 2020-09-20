285326253e0b86dc11def25c1c3edec8e9f69e0f
turzo267725 <turzo.dev@gmail.com>
2020-09-20 12:16:37 +0600

    notesql-ui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloLink } from "apollo-link";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import { onError } from "apollo-link-error";
+import Notifications, { notify } from "react-notify-toast";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message }) => notify.show(message, "error"));
+});
+
+const base = process.env.REACT_APP_API_URL;
+
+const httpLink = createHttpLink({
+  uri: `${base}/graphql`,
+});
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
+    <Notifications />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+serviceWorker.register();
